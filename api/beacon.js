3eb25e53dfc314ac3ac15f4d650f86d920c0dbef
Yavanika <53465556+yavanikab@users.noreply.github.com>
2026-06-05 18:53:38 +0530

    Updated

--- api/beacon.js
+++ api/beacon.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  var d = process.env.WS_EP;
+  var d = process.env.WS_Acknowledged_Logs;
   if (!d) { res.status(204).end(); return; }
   res.writeHead(302, { Location: d });
   res.end();
